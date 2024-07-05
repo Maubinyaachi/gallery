e586afa9a0d191cee9c3453b40c07bf1904d2cab
Maubinyaachi <maubi.nehemiah@student.moringaschool.com>
2024-07-05 15:58:44 +0300

    Add Jenkinsfile for CI/CD pipeline

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://maubinehemiah:kemmy1998@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://maubinehemiah:kemmy1998@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://maubinehemiah:kemmy1998@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+module.exports = {
+    mongoURI: 'mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.mongodb.net/<DBNAME>?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
